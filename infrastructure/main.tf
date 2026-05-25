156fa7959de077376029ad250bdaae3e316bbe27
devzwide <nxumalobukeka66@gmail.com>
2026-05-25 05:09:52 +0200

    Virtual Network Subnets and Network Security Groups Resource

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,6 +8,17 @@ resource "azurerm_resource_group" "rg-mrpapi-dev-southafricanorth" {
   }
 }
 
+resource "azurerm_network_security_group" "nsg-mrpapi-dev-southafricanorth" {
+  name                = "nsg-mrpapi-dev-southafricanorth"
+  resource_group_name = azurerm_resource_group.rg-mrpapi-dev-southafricanorth.name
+  location            = azurerm_resource_group.rg-mrpapi-dev-southafricanorth.location
+
+  tags = {
+    environment = "dev"
+    project     = "mrpapi"
+  }
+}
+
 resource "azurerm_virtual_network" "vnet-mrpapi-dev-southafricanorth" {
   name                = "vnet-mrpapi-dev-southafricanorth"
   resource_group_name = azurerm_resource_group.rg-mrpapi-dev-southafricanorth.name
@@ -18,4 +29,18 @@ resource "azurerm_virtual_network" "vnet-mrpapi-dev-southafricanorth" {
     environment = "dev"
     project     = "mrpapi"
   }
+}
+
+resource "azurerm_subnet" "subnet-mrpapi-api-dev-southafricanorth" {
+  name                 = "subnet-mrpapi-api-dev-southafricanorth"
+  resource_group_name  = azurerm_resource_group.rg-mrpapi-dev-southafricanorth.name
+  virtual_network_name = azurerm_virtual_network.vnet-mrpapi-dev-southafricanorth.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "subnet-mrpapi-db-dev-southafricanorth" {
+  name                 = "subnet-mrpapi-db-dev-southafricanorth"
+  resource_group_name  = azurerm_resource_group.rg-mrpapi-dev-southafricanorth.name
+  virtual_network_name = azurerm_virtual_network.vnet-mrpapi-dev-southafricanorth.name
+  address_prefixes     = ["10.0.2.0/24"]
 }

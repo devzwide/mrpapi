556b0242e2d6d7dc23be1d52097b8119e4ece1e0
devzwide <nxumalobukeka66@gmail.com>
2026-05-25 04:57:09 +0200

    Virtual Network Resource

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,3 +7,15 @@ resource "azurerm_resource_group" "rg-mrpapi-dev-southafricanorth" {
     project     = "mrpapi"
   }
 }
+
+resource "azurerm_virtual_network" "vnet-mrpapi-dev-southafricanorth" {
+  name                = "vnet-mrpapi-dev-southafricanorth"
+  resource_group_name = azurerm_resource_group.rg-mrpapi-dev-southafricanorth.name
+  location            = azurerm_resource_group.rg-mrpapi-dev-southafricanorth.location
+  address_space       = ["10.0.0.0/16"]
+
+  tags = {
+    environment = "dev"
+    project     = "mrpapi"
+  }
+}
